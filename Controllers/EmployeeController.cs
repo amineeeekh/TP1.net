1d306af613e2a73797e19d6796b2510eef752294
amine <amine14758789@gmail.com>
2024-11-05 14:14:43 +0100

    Initialisation git

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,100 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using tp1.Models;
+using TP1.Models.Repositories;
+using TP1.Models.Repositories.IRepository.tp1.Models.Repositories;
+
+
+namespace TP1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        readonly IRepository<Employee> employeeRepository;
+
+        public EmployeeController(IRepository<Employee> employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+        public ActionResult Search(string term)
+        {
+            var result = employeeRepository.Search(term);
+            return View("Index", result);
+        }
+        // GET: EmployeeController
+        public ActionResult Index()
+        {
+            var employees = employeeRepository.GetAll();
+            ViewData["EmployeesCount"] = employees.Count();
+            ViewData["SalaryAverage"] = employeeRepository.SalaryAverage();
+            ViewData["MaxSalary"] = employeeRepository.MaxSalary();
+            ViewData["HREmployeesCount"] = employeeRepository.HrEmployeesCount();
+
+            return View(employees);
+
+        }
+
+        // GET: EmployeeController/Details/5
+        public ActionResult Details(int id)
+        {
+            var employee = employeeRepository.FindByID(id);
+
+            return View(employee);
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+       
+
+        // GET: EmployeeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var employee = employeeRepository.FindByID(id);
+
+            return View(employee);
+
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Employee newemp)
+        {
+            try
+            {
+                employeeRepository.Update(id, newemp);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            employeeRepository.Delete(id);
+            return View();
+        }
+
+        // POST: EmployeeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace tp1.Models
+{
+    public class Employee : IEquatable<Employee>
+    {
+        public int Id { get; set; }
+        [Required, StringLength(20, ErrorMessage = "taille max 20 caracteres ")]
+        public string Name { get; set; }
+
+        [Required]
+        public string Departement { get; set; }
+
+        [Range(200, 5000)]
+        public int Salary { get; set; }
+
+        public bool Equals(Employee other)
+        {
+            return this.Id == other.Id;
+        }
+    }
+}
